360573e6cb918533d568c434078d39cebb42676b
Maximiliano Dalla Porta <gigamax13@gmail.com>
2018-09-27 02:43:26 -0300

    Adds class for error handling

---
+++ src/helpers/CustomError.js
@@ -0,0 +1,25 @@
+export default class CustomError {
+  static handler(res, err) {
+    err.stack && console.error(err.stack); // eslint-disable-line
+
+    if (err instanceof CustomError && (err.message && err.code)) {
+      const { code, message } = err;
+
+      res.status(code).send({
+        success: false,
+        message,
+      });
+    } else {
+      res.status(500).send({
+        success: false,
+        message: __('server.error.500'),
+      });
+    }
+  }
+
+  constructor(message, code = 500, stack) {
+    this.message = message;
+    this.code = code;
+    this.stack = stack;
+  }
+}
